6b11e0a9863804b2f464fe0463ae4931a6862bab
elikeith404 <ekbass44@gmail.com>
2022-07-20 17:41:07 -0400

    Changed formatting

--- models/book.js
+++ models/book.js
@@ -1,45 +1,47 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const bookSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String
-    },
-    publishDate: {
-        type: Date,
-        required: true
-    },
-    pageCount: {
-        type: Number
-    },
-    createdAt: {
-        type: Date,
-        required: true,
-        default: Date.now
-    },
-    coverImage: {
-        type: Buffer,
-        required: true
-    },
-    coverImageType: {
-        type: String,
-        required: true
-    },
-    author: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: 'Author'
-    }
+	title: {
+		type: String,
+		required: true,
+	},
+	description: {
+		type: String,
+	},
+	publishDate: {
+		type: Date,
+		required: true,
+	},
+	pageCount: {
+		type: Number,
+	},
+	createdAt: {
+		type: Date,
+		required: true,
+		default: Date.now,
+	},
+	coverImage: {
+		type: Buffer,
+		required: true,
+	},
+	coverImageType: {
+		type: String,
+		required: true,
+	},
+	author: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: true,
+		ref: 'Author',
+	},
 });
 
-bookSchema.virtual('coverImagePath').get(function() {
-    if(this.coverImage != null && this.coverImageType != null){
-        return `data:${this.coverImageType};charset=utf-8;base64,${this.coverImage.toString('base64')}`
-    }
-})
+bookSchema.virtual('coverImagePath').get(function () {
+	if (this.coverImage != null && this.coverImageType != null) {
+		return `data:${
+			this.coverImageType
+		};charset=utf-8;base64,${this.coverImage.toString('base64')}`;
+	}
+});
 
 const bookModel = mongoose.model('Book', bookSchema);
 export default bookModel;
